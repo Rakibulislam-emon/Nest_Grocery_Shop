d81b31b21485d89b497ecf7594f441c6c00da518
Rakibulislam-emon <rakibulislamemon60>
2024-12-15 02:34:56 +0600

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Container from "@/components/Container";
+import Header from "@/components/Header/Header";
 import React from "react";
 
 export default function page() {
   return (
     <Container>
       <h1 className="text-5xl">Hello, Next.js!</h1>
+      <Header/>
     </Container>
   );
 }
