e97f99aad6c5e62dc2a43c427cf4dcc3ab7486e9
Rakibulislam-emon <rakibulislamemon60>
2024-12-15 02:00:39 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Container from "@/Components/Container";
 import Header from "@/Components/Header/Header";
-import { Banner } from "@/Components/HeroSection/BannerAndCategory/Banner";
+// import { Banner } from "@/Components/HeroSection/BannerAndCategory/Banner";
 import React from "react";
 
 export default function page() {
   return <Container>
     <Header/>
-    <Banner/>
+    {/* <Banner/> */}
   </Container>;
 }
