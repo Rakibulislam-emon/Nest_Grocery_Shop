fc599e49bcc6501e03911e8bd0498b7fd1eb031f
Rakibulislam-emon <rakibulislamemon60>
2024-12-15 02:18:49 +0600

    header add

--- src/Components/Shared/StyledButton.jsx
+++
@@ -1,7 +0,0 @@
-import './Button.css'
-
-export default function StyledButton({children}) {
-  return (
-    <button  className='styledButton w-full flex justify-between'>{children}</button>
-  )
-}
